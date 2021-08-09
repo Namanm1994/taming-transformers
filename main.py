4a2f216b6e331a333e92feb4c6357dbb5d44adbc
Robert A. Gonsalves <robgonsalves@gmail.com>
2021-08-09 11:50:39 -0400

    remove a print statement

--- main.py
+++ main.py
@@ -516,7 +516,6 @@ if __name__ == "__main__":
         callbacks_cfg = OmegaConf.create()
         callbacks_cfg = OmegaConf.merge(default_callbacks_cfg, callbacks_cfg)
         trainer_kwargs["callbacks"] = [instantiate_from_config(callbacks_cfg[k]) for k in callbacks_cfg]
-        print(trainer_kwargs)
 
         trainer = Trainer.from_argparse_args(trainer_opt, **trainer_kwargs)
 
